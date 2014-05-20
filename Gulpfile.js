e94457b45f6160791f13086d3f506bf4b431335e
Chip Bell <chipbell4@gmail.com>
2014-05-19 23:19:34 -0400

    Making task dependencies a little smarter

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,14 @@ var mocha = require('gulp-mocha');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('dist', function() {
+gulp.task('lint', function() {
+	return gulp.src('src/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('dist', ['lint', 'mocha'], function() {
 
 	// Get the paths to application files
 	var application_file_paths = ['peach', 'peach.drawable', 'peach.input', 'peach.primitive'].map(function(file) {
@@ -13,7 +20,6 @@ gulp.task('dist', function() {
 	});
 
 	return gulp.src(application_file_paths)
-		.pipe(jshint())
 		.pipe(concat('peach.js'))
 		.pipe(gulp.dest('dist'))
 		.pipe(rename({ suffix: '.min' }))
@@ -22,9 +28,9 @@ gulp.task('dist', function() {
 		.pipe(concat('peach.min.js'));
 });
 
-gulp.task('mocha', function() {
+gulp.task('mocha', ['lint'], function() {
 	return gulp.src('tests/*.js')
 		.pipe(mocha());
 });
 
-gulp.task('default', ['mocha', 'dist']);
+gulp.task('default', ['mocha', 'lint', 'dist']);
