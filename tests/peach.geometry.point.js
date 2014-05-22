24b6dbc0c2ce5e835473d2ee984eaa104beceff9
Chip Bell <chipbell4@gmail.com>
2014-05-22 12:20:27 -0400

    Adding a test helper function

--- tests/peach.geometry.point.js
+++ tests/peach.geometry.point.js
@@ -1,4 +1,10 @@
 describe('Peach.Geometry.Point', function() {
+
+	// Helper fuzzy compare function
+	var expectToBeRouglyEqual = function(value, expected) {
+		expect( Math.abs( value - expected ) < 0.00001 ).to.be.ok;
+	}
+
 	it('Should exist on the geometry object', function() {
 		expect(Peach.Geometry.Point).to.be.ok;
 	});
@@ -14,8 +20,7 @@ describe('Peach.Geometry.Point', function() {
 		var p = Peach.Geometry.Point.fromPolar(2, Math.PI / 2);
 
 		// Use fuzzy comparison for values, due to roundoff
-		var EPSILON = 0.00001;
-		expect( Math.abs(p.x) < EPSILON ).to.be.ok;
-		expect(Math.abs(p.y - 2) < EPSILON ).to.be.ok;
+		expectToBeRouglyEqual(p.x, 0);
+		expectToBeRouglyEqual(p.y, 2);
 	})
 });
