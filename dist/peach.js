42ed496e59f50a3c7d00f8836e000766b00f17c6
Chip Bell <chipbell4@gmail.com>
2014-05-28 11:43:36 -0400

    Adding a rectangle class

--- dist/peach.js
+++ dist/peach.js
@@ -309,6 +309,38 @@ Peach.Geometry.Point = (function() {
 	return Point;
 })();
 
+Peach.Geometry.Rectangle = (function() {
+	/**
+	 * Creates a new Rectangle, with the specified corners
+	 */
+	var Rectangle = function(corner1, corner2) { 
+
+		var min_x = Math.min(corner1.x, corner2.x);
+		var min_y = Math.min(corner1.y, corner2.y);
+		var max_x = Math.max(corner1.x, corner2.x);
+		var max_y = Math.max(corner1.y, corner2.y);
+
+		// set the corner fields
+		this.top_left = Peach.Geometry.Point.fromCartesian(min_x, min_y);
+		this.top_right = Peach.Geometry.Point.fromCartesian(max_x, min_y);
+		this.bottom_left = Peach.Geometry.Point.fromCartesian(min_x, max_y);
+		this.bottom_right = Peach.Geometry.Point.fromCartesian(max_x, max_y);
+
+		// set the width and height fields
+		this.width = max_x - min_x;
+		this.height = max_y - min_y;
+	};
+
+	/**
+	 * Simple clipping for the rectangle (scissoring)
+	 */
+	Rectangle.prototype.contains = function(point) {
+		return this.top_left.x <= point.x && point.x <= this.top_right.x && this.top_left.y <= point.y && point.y <= this.bottom_left.y;
+	};
+
+	return Rectangle;
+})();
+
 /**
  * A set of handy primitive drawing methods
  * TODO: Expand these a little
