cabba4a3c4e6547d0da9d6159b8caa27d7fc300c
Chip Bell <cbell@pbs.org>
2018-05-05 17:27:12 -0400

    Collision detection

--- index.js
+++ index.js
@@ -44,6 +44,12 @@
     this.bitmap = bitmap;
     this.x = 0;
     this.y = 0;
+    this.width = this.bitmap.length;
+    this.height = this.bitmap[0].length;
+  };
+
+  ArcadeRenderer.Sprite.prototype.collides = function(s) {
+    return this.x < s.x + s.width && this.x + this.width > s.x && this.y < s.y + s.height && this.y + this.height > s.y;
   };
 
   window.ArcadeRenderer = ArcadeRenderer;
