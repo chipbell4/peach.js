95848135de08dd269036bdebf31c6c19830759a4
Chip Bell <chipbell4@gmail.com>
2019-08-29 11:21:35 +0200

    Adding babel for old browser support

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/peach.js',
@@ -8,6 +9,11 @@ export default {
     name: 'Peach'
   },
   plugins: [
+    babel({
+      presets: [
+        ['@babel/preset-env', { targets: { ie: '11' } }]
+      ]
+    }),
     terser()
   ]
 }
