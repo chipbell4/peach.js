c77a83608602f0ed31472e3ae4a9ec16fa32007b
Chip Bell <chipbell4@gmail.com>
2019-08-31 13:33:50 +0200

    Fixing a bug around sprite size calculation
    If the bitmap is swapped out after instantiation, the width and height
    would become stale and cause collision to not work properly

--- src/sprite.js
+++ src/sprite.js
@@ -1,19 +1,25 @@
-const Sprite = function(bitmap, paletteAssignment) {
-  this.bitmap = bitmap;
-  this.paletteAssignment = paletteAssignment;
-  this.x = 0;
-  this.y = 0;
-  this.width = this.bitmap.length;
-  this.height = this.bitmap[0].length;
-};
+export default class Sprite {
+  constructor(bitmap, paletteAssignment) {
+    this.bitmap = bitmap;
+    this.paletteAssignment = paletteAssignment;
+    this.x = 0;
+    this.y = 0;
+  }
 
-Sprite.prototype.collides = function(s) {
-  return (
-    this.x < s.x + s.width &&
-    this.x + this.width > s.x &&
-    this.y < s.y + s.height &&
-    this.y + this.height > s.y
-  );
-};
+  get width() {
+    return this.bitmap[0].length;
+  }
 
-export default Sprite;
+  get height() {
+    return this.bitmap.length;
+  }
+
+  collides(s) {
+    return (
+      this.x < s.x + s.width &&
+        this.x + this.width > s.x &&
+        this.y < s.y + s.height &&
+        this.y + this.height > s.y
+    );
+  }
+}
