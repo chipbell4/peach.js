fc28c74204a6f798ce03fc073d0e48b0b3eed616
Chip Bell <chipbell4@gmail.com>
2026-05-06 22:13:19 -0400

    typing

--- editor/src/code.tsx
+++ editor/src/code.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Code = ({ sprite }) => {
+interface CodeProps {
+    sprite: (number | null)[][];
+}
+
+const Code = ({ sprite }: CodeProps) => {
     return (
         <div className="code-container">
             <pre className="rendered-code">
